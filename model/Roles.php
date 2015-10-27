f69abb1035d4d103a2d2c756947a9bb861a339a5
Stefano Chiesa <base278@gmail.com>
2015-10-27 18:50:42 +0800

    Add roles class

---
+++ model/Roles.php
@@ -0,0 +1,14 @@
+<?php
+
+class Roles {
+  public static $ADMIN = "admin";
+  public static $CREATOR = "creator";
+  public static $CONTRIBUTOR = "contributor";
+
+  public static function getUserList($roles) {
+
+  }
+
+}
+
+?>
