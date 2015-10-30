c53f17668f72db99e83f3543ea2c58abca0ca2e5
raisfathin <rais.fathin38@gmail.com>
2015-10-30 17:01:37 +0800

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -4,39 +4,26 @@ DROP TABLE fundit_user CASCADE CONSTRAINTS;
 DROP TABLE fundit_roles CASCADE CONSTRAINTS;
 DROP SEQUENCE fundit_contribution_seq;
 DROP SEQUENCE fundit_project_seq;
-DROP SEQUENCE fundit_user_seq;
 
-CREATE TABLE fundit_roles (name VARCHAR(10) PRIMARY KEY);
+CREATE TABLE fundit_roles (name VARCHAR(11) PRIMARY KEY);
 
 CREATE TABLE fundit_user (
-  id INT PRIMARY KEY,
-  roles VARCHAR(10) NOT NULL,
+  username VARCHAR(20) PRIMARY KEY,
+  roles VARCHAR(11) NOT NULL,
   name VARCHAR(32) NOT NULL,
-  email VARCHAR(32) NOT NULL,
+  email VARCHAR(32) NOT NULL UNIQUE,
   password VARCHAR(32) NOT NULL,
   FOREIGN KEY (roles) REFERENCES fundit_roles(name) ON DELETE CASCADE
 );
-  
-CREATE SEQUENCE fundit_user_seq;
 
-CREATE OR REPLACE TRIGGER fundit_user_bir
-BEFORE INSERT ON fundit_user
-FOR EACH ROW
-
-BEGIN
-  SELECT fundit_user_seq.NEXTVAL
-  INTO :new.id
-  FROM dual;
-END;
-/
 CREATE TABLE fundit_project (
   id INT PRIMARY KEY,
-  owner_id INT NOT NULL,
+  owner_id VARCHAR(20) NOT NULL,
   title VARCHAR(64) NOT NULL,
   description VARCHAR(64) NOT NULL,
   goal REAL NOT NULL,
   deadline TIMESTAMP,
-  FOREIGN KEY (owner_id) REFERENCES fundit_user(id) ON DELETE CASCADE
+  FOREIGN KEY (owner_id) REFERENCES fundit_user(username) ON DELETE CASCADE
 );
   
 CREATE SEQUENCE fundit_project_seq;
@@ -54,10 +41,10 @@ END;
 
 CREATE TABLE fundit_contribution (
   id INT PRIMARY KEY,
-  contributor_id INT NOT NULL,
+  contributor_id VARCHAR(20) NOT NULL,
   project_id INT NOT NULL,
   amount REAL NOT NULL,
-  FOREIGN KEY (contributor_id) REFERENCES fundit_user(id) ON DELETE CASCADE,
+  FOREIGN KEY (contributor_id) REFERENCES fundit_user(username) ON DELETE CASCADE,
   FOREIGN KEY (project_id) REFERENCES fundit_project(id) ON DELETE CASCADE
 );
   
@@ -73,3 +60,12 @@ BEGIN
   FROM dual;
 END;
 /
+
+INSERT INTO fundit_roles VALUES('admin');
+INSERT INTO fundit_roles VALUES('contributor');
+INSERT INTO fundit_roles VALUES('creator');
+--INSERT INTO fundit_user (name, roles, email, password) VALUES('budi', 'admin', 'budi@anduk.com', 'koyo');
+--SELECT * FROM fundit_user WHERE name = 'budi';
+--DELETE FROM fundit_user WHERE name = 'budi';
+--SELECT fundit_user_seq.CURRVAL FROM dual;
+
