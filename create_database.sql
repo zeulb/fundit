c9eef083eaa9a7afe63718737e9dffc06c3b297b
raisfathin <rais.fathin38@gmail.com>
2015-10-31 14:45:35 +0800

    Update create database script

--- create_database.sql
+++ create_database.sql
@@ -43,6 +43,7 @@ CREATE TABLE fundit_contribution (
   id INT PRIMARY KEY,
   contributor VARCHAR(20) NOT NULL,
   project_id INT NOT NULL,
+  timestamp TIMESTAMP NOT NULL,
   amount REAL NOT NULL,
   FOREIGN KEY (contributor) REFERENCES fundit_user(username) ON DELETE CASCADE,
   FOREIGN KEY (project_id) REFERENCES fundit_project(id) ON DELETE CASCADE
@@ -69,8 +70,16 @@ INSERT INTO fundit_roles VALUES('creator');
 --DELETE FROM fundit_user WHERE name = 'budi';
 --SELECT fundit_user_seq.CURRVAL FROM dual;
 INSERT INTO fundit_user (username, name, email, roles, password) VALUES ('budi', 'budi_bola', 'budi@bola.com', 'admin', '529ca8050a00180790cf88b63468826a');
+INSERT INTO fundit_project (owner, title, description, goal, deadline) VALUES ('budi', 'bolpen untuk irvin', 'beliin bolpen', 800, '31-OCT-15 02.23.04.000000000 AM');
 INSERT INTO fundit_user (username, name, email, roles, password) VALUES ('a', 'a', 'a', 'contributor', '529ca8050a00180790cf88b63468826a');
 
 SELECT * FROM fundit_user WHERE username = 'budi';
 SELECT * FROM fundit_user;
 SELECT * FROM fundit_user;
+
+  id INT PRIMARY KEY,
+  owner VARCHAR(20) NOT NULL,
+  title VARCHAR(64) NOT NULL,
+  description VARCHAR(64) NOT NULL,
+  goal REAL NOT NULL,
+  deadline TIMESTAMP NOT NULL,
