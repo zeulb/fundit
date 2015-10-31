a71c439f1c347a6589c405974c0e1d816be55c17
raisfathin <rais.fathin38@gmail.com>
2015-10-31 16:34:40 +0800

    Add timezone to DateHelper

--- helper/DateHelper.php
+++ helper/DateHelper.php
@@ -4,8 +4,10 @@ namespace DateHelper {
 
 include_once __DIR__ . '/../db/DBHelper.php';
 
+date_default_timezone_set("Asia/Singapore");
+
 function beautifyDateFromSql($dateFromSql) {
-  $result = DateTime::createFromFormat('j-M-y h.i.s.u A', $dateFromSql);
+  $result = date_create_from_format('j-M-y h.i.s.u A', $dateFromSql);
   $result = $result->format('d M Y h:m A');
   return $result;
 }
