a4485badd64e31a50843fe7bf03d8fd510df35bd
raisfathin <rais.fathin38@gmail.com>
2015-10-31 13:55:12 +0800

    Fix date format to conform with oracle sql format

--- controller/ContributionController.php
+++ controller/ContributionController.php
@@ -1,14 +1,17 @@
 <?php
 namespace ContributionController {
 
-include_once __DIR__ . '../model/Contribution.php';
-include_once __DIR__ . '../db/DBHandler.php';
+include_once __DIR__ . '/../model/Contribution.php';
+include_once __DIR__ . '/../db/DBHandler.php';
 
 function createNewContribution($projectId, $amount) {
   date_default_timezone_set("Asia/Singapore");
 
   $contributor = $_SESSION['username'];
-  $contributionDate = date("Y-m-d H:i:s");
+  $contributionDate = date("d-M-yh:i:s.u", time());
+  $inner = "TO_TIMESTAMP('{$contributionDate}','DD-MON-RRHH24:MI:SS.FF')";
+  $statement = "SELECT {$inner} FROM dual";
+  $contributionDate = \DBHandler::execute($statement, true)[0][strtoupper($inner)];
   $amount = floatval($amount);
   $projectId = intval($projectId);
 
