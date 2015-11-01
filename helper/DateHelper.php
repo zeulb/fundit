4bd2c4df7fa9c7c5501a5f3fb981e1f327716c8c
raisfathin <rais.fathin38@gmail.com>
2015-11-01 15:58:08 +0800

    Fix date parsing

--- helper/DateHelper.php
+++ helper/DateHelper.php
@@ -8,7 +8,7 @@ date_default_timezone_set("Asia/Singapore");
 
 function beautifyDateFromSql($dateFromSql) {
   $result = date_create_from_format('j-M-y h.i.s.u A', $dateFromSql);
-  $result = $result->format('d M Y g:i A');
+  $result = $result->format('d M Y h:i A');
   return $result;
 }
 
